eda48f569e6675243ed42f92b85127648d546d4f
ilosey14 <56739322+ilosey14@users.noreply.github.com>
2019-12-18 23:45:41 -0500

    Update popup.js
    Replaced tabs query promise with callback argument

--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,33 @@
 (function () {
     // toggle button
     var theme = document.getElementById('theme-toggle');
+
     theme.onchange = function () {
         browser.storage.local.set({dark: this.checked});
     };
-    theme.checked = browser.storage.local.get('dark', function (response) {
+
+    theme.checked = browser.storage.local.get('dark', response => {
         theme.checked = response.dark;
         delete theme;
     });
 
     // whitelist
     document.getElementById('whitelist-page').onclick = function () {
-        browser.storage.local.get('whitelist', function (response) {
+        browser.storage.local.get('whitelist', response => {
             if (!(response.whitelist instanceof Array)) return;
-            response.whitelist.push(location.host);
-            browser.storage.local.set({whitelist: response.whitelist});
+
+            browser.tabs.query({
+                currentWindow: true,
+                active: true
+            }, tabs => {
+                var url = new URL(tabs[0].url);
+
+                response.whitelist.push(url.origin);
+                browser.storage.local.set({ whitelist: response.whitelist });
+            });
         });
     };
+
     document.getElementById('whitelist-edit').onclick = function () {
         // browser.runtime.openOptionsPage();
         browser.tabs.create({ url: '/options/options.html'});
