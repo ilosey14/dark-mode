38fa3b6ebd85b6de45d2cb440843d123d2f420b7
ilosey14 <56739322+ilosey14@users.noreply.github.com>
2019-12-18 22:43:31 -0500

    Add files via upload

---
+++ options/options.js
@@ -0,0 +1,56 @@
+// ./settings/options.js
+var list = document.getElementById('whitelist');
+list.nItems = 0;
+
+// get whitelist
+browser.storage.local.get('whitelist', function (result) {
+    if (!(result.whitelist instanceof Array)) return;
+
+    result.whitelist.forEach(addItem);
+});
+
+// add item button
+document.getElementById('whitelist-add-item').onclick = function () {
+    addItem();
+};
+
+// save settings
+document.getElementById('whitelist-form').onsubmit = function (e) {
+    e.preventDefault();
+
+    // get new list
+    var whitelist = [];
+
+    (new FormData(e.target)).forEach(function (text) {
+        if (!text) return;
+        whitelist.push(text);
+    });
+
+    // set storage
+    browser.storage.local.set({
+        whitelist: whitelist
+    });
+};
+
+function addItem(value = '') {
+    var li = document.createElement('li'),
+        text = document.createElement('input'),
+        remove = document.createElement('input');
+
+    text.type = 'text';
+    text.name = 'item-' + list.nItems++;
+    text.value = value;
+    text.className = 'whitelist-item';
+    li.appendChild(text);
+
+    remove.type = 'button';
+    remove.value = 'x';
+    remove.onclick = function () {
+        this.parentElement.parentElement.removeChild(this.parentElement);
+    };
+    li.appendChild(remove);
+
+    list.appendChild(li);
+
+    text.focus();
+}
